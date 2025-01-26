ad46bc3cc747d453f7580116e95ecaf9b2d659be
Carter Spear <90937813+carteraspear@users.noreply.github.com>
2025-01-26 15:34:03 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import { useAuth } from 'react-oidc-context';
 
@@ -7,8 +6,8 @@ function App() {
 
   const signOutRedirect = () => {
     const clientId = 'm2rogeivplk4o2tccvkngrk3c';
-    const logoutUri = '<logout uri>'; // Replace with your actual logout URI
-    const cognitoDomain = 'https://<user pool domain>'; // Replace with your Cognito domain
+    const logoutUri = 'https://d84l1y8p4kdic.cloudfront.net'; // Replace with your actual logout URI
+    const cognitoDomain = 'https://us-east-2f8bywcth0.auth.us-east-2.amazoncognito.com'; // Replace with your Cognito domain
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
@@ -23,11 +22,9 @@ function App() {
   if (auth.isAuthenticated) {
     return (
       <div>
-        <h1>Welcome!</h1>
-        <p>Hello: {auth.user?.profile.email}</p>
-        <p>ID Token: {auth.user?.id_token}</p>
-        <p>Access Token: {auth.user?.access_token}</p>
-        <p>Refresh Token: {auth.user?.refresh_token}</p>
+        <h1>Welcome to Playdate!</h1>
+        <p>Hello, {auth.user?.profile.email}!</p>
+        <p>You have successfully signed in.</p>
         <button onClick={() => auth.removeUser()}>Sign out</button>
       </div>
     );
