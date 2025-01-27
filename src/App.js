9d2cfaa2d202a5e58c4c68b009fd9dfe6f04b8d3
Carter Spear <90937813+carteraspear@users.noreply.github.com>
2025-01-26 16:19:42 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,47 +1,47 @@
 import './App.css';
 import { useAuth } from 'react-oidc-context';
 
-function App() {
+function playdate() {
   const auth = useAuth();
 
   const signOutRedirect = () => {
-    const clientId = 'm2rogeivplk4o2tccvkngrk3c';
-    const logoutUri = 'https://d84l1y8p4kdic.cloudfront.net'; // Replace with your actual logout URI
-    const cognitoDomain = 'https://us-east-2f8bywcth0.auth.us-east-2.amazoncognito.com'; // Replace with your Cognito domain
+    const clientId = 'clientidhere';
+    const logoutUri = 'thisFieldHasBeenAVarietyOfThings'; 
+    const cognitoDomain = 'myactualcognitodomainhere'; 
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
   if (auth.isLoading) {
-    return <div>Loading...</div>;
+    return <div>loading...</div>;
   }
 
   if (auth.error) {
-    return <div>Encountering error... {auth.error.message}</div>;
+    return <div>Error... {auth.error.message}</div>;
   }
 const signInRedirect = () => {
-  const redirectUri = 'https://main.d3vjsffdail7l2.amplifyapp.com/'; // Set this to your local development URL
+  const redirectUri = 'thisFieldHasBeenAVarietyOfThings';
   auth.signinRedirect({ redirectUri });
 };
 
   if (auth.isAuthenticated) {
     return (
       <div>
-        <h1>Welcome to Playdate!</h1>
-        <p>Hello, {auth.user?.profile.email}!</p>
-        <p>You have successfully signed in.</p>
-        <button onClick={() => auth.removeUser()}>Sign out</button>
-        <button>Click Me</button> {/* This button only appears when authenticated */}
+        <h1>playdateplaydateplaydate</h1>
+        <p>hello  {auth.user?.profile.email}!</p>
+        <p>sign in worked</p>
+        <button onClick={() => auth.removeUser()}>sign out</button>
+        <button>this button should appear if auth successfully redirected </button> 
       </div>
     );
   }
 
   return (
     <div>
-      <h1>Main brand page here</h1>
-      <button onClick={() => auth.signinRedirect()}>Sign in</button>
-      <button onClick={() => signOutRedirect()}>Sign out</button>
+      <h1>main sign in page here</h1>
+      <button onClick={() => auth.signinRedirect()}>sign in</button>
+      <button onClick={() => signOutRedirect()}>sign out</button>
     </div>
   );
 }
 
-export default App;
+export default playdate;
