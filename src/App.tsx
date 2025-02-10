18345f1195fb859cdf13026101f767fb023d5dcc
Carter Spear <90937813+carteraspear@users.noreply.github.com>
2025-02-10 14:26:35 -0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React, { useState } from 'react';
 import { useAuth } from 'react-oidc-context';
 import './App.css';
 
@@ -9,7 +8,6 @@ interface UserProfile {
 
 function App() {
   const auth = useAuth();
-  const [isSignedIn, setIsSignedIn] = useState<boolean>(false);
 
   const handleSignIn = () => {
     auth.signinRedirect(); // Redirect to Cognito for sign-in
@@ -17,14 +15,6 @@ function App() {
 
   const handleSignOut = () => {
     auth.removeUser(); // Clear the user session
-    setIsSignedIn(false);
-  };
-
-  const signOutRedirect = () => {
-    const clientId = "39n6eqjmmo691695nelad9bbt";
-    const logoutUri = "https://example.com"; // Replace with your logout URI
-    const cognitoDomain = "https://cognito-idp.us-east-2.amazonaws.com/us-east-2_XohcmASr2"; // Replace with your Cognito domain
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
   };
 
   if (auth.isLoading) {
